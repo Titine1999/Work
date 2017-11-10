f6194150c6a10d364eb10789426e7f88a404bd46
Dupuis VALENTINE <dvalenti@e1r5p10.42.fr>
2017-11-10 02:01:14 +0100

    Second Push

--- libft/ft_strstr.c
+++ libft/ft_strstr.c
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dvalenti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/08 03:39:39 by dvalenti          #+#    #+#             */
-/*   Updated: 2017/11/10 01:02:04 by dvalenti         ###   ########.fr       */
+/*   Created: 2017/11/10 00:26:01 by dvalenti          #+#    #+#             */
+/*   Updated: 2017/11/10 01:55:16 by dvalenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_strcmp(const	char *s1, const char *s2)
+int		ft_strcmp(const char *s1, const char *s2)
 {
 	unsigned int	i;
 
@@ -18,4 +18,21 @@ int		ft_strcmp(const	char *s1, const char *s2)
 	while (s1[i] && s1[i] == s2[i])
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
+
+char	*ft_strstr(const char *haystack, const char *needle)
+{
+	int		i;
+
+	i = 0;
+	if (needle[0] == '\0')
+		return (haystack);
+	while (*haystack)
+	{
+		if (!(ft_strcmp(haystack, needle)
+			return (needle);
+		haystack++;
+	}
+return (0);
 }
